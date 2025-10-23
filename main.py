1f170dafa923e0406ff96108a9c97fe80f1bda44
Manaila Cristian <crmanaila@gmail.com>
2025-10-24 00:19:08 +0300

    Update Product names and quantities; remove inventory display lines.

--- main.py
+++ main.py
@@ -6,20 +6,14 @@ def main():
     manager = ProductManager()
     
     
-    product1 = Product("Laptop", 999.99, 10)
-    product2 = Product("Smartphone", 499.49, 25)
-    product3 = Product("Headphones", 79.99, 50)
+    product1 = Product("Tablet", 90.99, 20)
+    product2 = Product("Smartwatch", 199.99, 19)
+    product3 = Product("Camera", 299.99, 15)
     
     manager.add_product(product1)
     manager.add_product(product2)
     manager.add_product(product3)
     
-    
-    manager.display_all_products()
-    
-    
-    total_value = manager.total_inventory_value()
-    print(f"\n Total Inventory Value: ${total_value:.2f}")
     
 if __name__ == "__main__":
     main()
